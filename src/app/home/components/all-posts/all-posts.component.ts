da43c37da8fcb23ae3173c2bf6b46d9d11541c78
VegasDevApps <rico777@gmail.com>
2022-12-08 07:02:47 +0200

    Feeds scroll

--- src/app/home/components/all-posts/all-posts.component.ts
+++ src/app/home/components/all-posts/all-posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Post } from '../../models/Post.model';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-all-posts',
@@ -7,8 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllPostsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  ngOnInit() {}
+  queryParams: string;
+  allLoadedPosts: Post[] = [];
+  numberOfPosts =5;
+  skipPost = 0;
 
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.getPosts(null);
+  }
+
+  getPosts(event){
+    if(this.skipPost === 20){
+      event.target.disabled = true;
+    }
+    this.queryParams = `?take=${this.numberOfPosts}&skip=${this.skipPost}`;
+
+    this.postService.getSelectedPosts(this.queryParams).subscribe((posts: Post[]) => {
+      this.allLoadedPosts = [...this.allLoadedPosts, ...posts];
+      
+      if(event) event.target.complete();
+      this.skipPost += 5;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  loadData(event){
+    this.getPosts(event);
+  }
 }
